7e16ab65f7467622f35902b67a916f1110fe1ba3
Vagiz Musin <vagiz_mus@mail.ru>
2021-01-02 23:00:24 +0600

    feat: add webpack

---
+++ webpack.config.js
@@ -0,0 +1,114 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetWebpackPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+
+const isDev = process.env.NODE_ENV === "development";
+const isProd = !isDev;
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: "all",
+    },
+  };
+  if (isProd) {
+    config.minimizer = [
+      new OptimizeCssAssetWebpackPlugin(),
+      new TerserWebpackPlugin(),
+    ];
+  }
+  return config;
+};
+
+const filename = (ext) => {
+  return isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
+};
+
+console.log("IS DEV", isDev);
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  mode: "development",
+  entry: {
+    main: "./index.js",
+  },
+  output: {
+    filename: filename("js"),
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".js", ".json", "png", "jpg"],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  optimization: optimization(),
+  devServer: {
+    open: true,
+    port: 4200,
+    hot: isDev,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./index.html",
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/img/favicon.ico"),
+          to: path.resolve(__dirname, "dist/img"),
+        },
+        {
+          from: path.resolve(__dirname, "src/img"),
+          to: path.resolve(__dirname, "dist/img"),
+        },
+        
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: filename("css"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "",
+            },
+          },
+          "css-loader",
+        ],
+      },
+      {
+        test: /\.(s[ac]ss)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "",
+            },
+          },
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+};
